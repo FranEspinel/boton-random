90f19b0999fc92b48c7854c0fb76c90c96a9b9bf
FranEspinel <fran.espinel@gmail.com>
2023-12-09 18:15:11 -0300

    Cambios

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,34 @@
+import { useState } from "react";
 import Boton from "./components/Boton";
 import { getRandomPos } from "./components/Boton";
+
 function App() {
+  const [pos, setPos] = useState(getRandomPos);
+  const [cont, setCont] = useState(0);
+  const [backgroundColor, setBackgoundColor] = useState("#ffffff");
+  const handleButtonClick = () => {
+    const newPos = getRandomPos();
+    setPos(newPos);
+    setCont((prevValue) => prevValue + 1);
+    setBackgoundColor(getRandomColor());
+    console.log(newPos, cont);
+  };
+  const getRandomColor = () => {
+    return "#" + Math.floor(Math.random() * 16777215).toString(16);
+  };
+
+  const pageStyle: React.CSSProperties = {
+    backgroundColor: backgroundColor,
+    height: "100vh",
+  };
   return (
-    <div>
-      <Boton color="danger" onClick={() => console.log(getRandomPos())}></Boton>
+    <div style={pageStyle}>
+      <Boton
+        texto={cont}
+        color="danger"
+        posicion={pos}
+        onClick={handleButtonClick}
+      ></Boton>
     </div>
   );
 }
--- src/components/Boton.tsx
+++ src/components/Boton.tsx
@@ -1,4 +1,5 @@
 interface Props {
+  texto: number;
   color: string;
   posicion?: { top?: number; left?: number };
   onClick: () => void;
@@ -7,19 +8,28 @@ interface Props {
 export const getRandomPos = (): { top: number; left: number } => {
   let top = Math.floor(Math.random() * window.innerHeight);
   let left = Math.floor(Math.random() * window.innerWidth);
-
+  if (top > window.innerHeight - 300) {
+    top -= 150;
+  }
+  if (left > window.innerWidth - 300) {
+    left -= 150;
+  }
   return { top, left };
 };
 
-const Boton = ({ color, posicion, onClick }: Props) => {
+const Boton = ({ texto, color, posicion, onClick }: Props) => {
   const containerStyle: React.CSSProperties = {
     position: "absolute",
     ...(posicion || getRandomPos()),
   };
+  const handleButtonClick = () => {
+    onClick();
+  };
+
   return (
     <div style={containerStyle}>
-      <button className={"btn btn-outline-" + color} onClick={onClick}>
-        posicion
+      <button className={"btn btn-" + color} onClick={handleButtonClick}>
+        {texto}
       </button>
     </div>
   );
